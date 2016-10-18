67997ad3420338d7cc55a689e12906c9374de2b7
Adam <fidler2326@gmail.com>
2016-10-18 20:46:28 +0100

    Update email config

--- app.rb
+++ app.rb
@@ -34,8 +34,8 @@ def configure_pony
     :via_options => {
       :address              => 'smtp.sendgrid.net',
       :port                 => '587',
-      :user_name            => ENV['fidler2326'],
-      :password             => ENV['Yewaka170!'],
+      :user_name            => ENV['SENDGRID_USERNAME'],
+      :password             => ENV['SENDGRID_PASSWORD'],
       :authentication       => :plain,
       :enable_starttls_auto => true,
       :domain               => 'heroku.com'
