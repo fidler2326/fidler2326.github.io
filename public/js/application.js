e39b5168d3094937a63695e5f9790c2a89380707
Adam <fidler2326@gmail.com>
2016-10-21 19:02:41 +0100

    Add scrollspy

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,11 @@
+// Header particles
+// ----------------
 particlesJS.load('particles-js', '/js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
 
+// Fixed nav
+// ---------
 var navPosition = $('nav.main').offset().top;
 $(window).scroll(function () {
   if ($(window).scrollTop() > navPosition) {
@@ -11,6 +15,40 @@ $(window).scroll(function () {
   }
 });
 
+// Scrollspy
+// ---------
+$(document).ready(function(){
+  var sections = [];
+  var scrolled_id = false;
+  var id = false;
+  var $navBar = $('nav.main ul');
+  var $navLink = $navBar.find('.jump-to');
+
+  $navLink.each(function(){
+  	sections.push($($(this).attr('href')));
+  });
+
+  $(window).scroll(function(e){
+    e.preventDefault();
+    var scrollTop = $(this).scrollTop() + ($(window).height() / 3);
+
+    for(var i in sections){
+    	var section = sections[i];
+      if(scrollTop > section.offset().top){
+  			scrolled_id = section.attr('id');
+      }
+
+      if(scrolled_id !== id){
+        id = scrolled_id;
+        $navLink.removeClass('active');
+        $('a[href="#'+ id + '"]', $navBar).addClass('active');
+      }
+    }
+  });
+  $(window).trigger('scroll');
+});
+
+//
 function checkWidth() {
 	  var windowsize = $(window).width();
 	  if (windowsize < 720) {
