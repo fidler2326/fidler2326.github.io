18f42513ffb835c666788b0e135fc52d440c7108
Adam <fidler2326@gmail.com>
2017-01-12 21:36:12 +0000

    Start layout rework

--- public/js/application.js
+++ public/js/application.js
@@ -129,3 +129,49 @@ $(document).ready(function(){
 $(window).resize(function() {
   checkWidth();
 });
+
+
+// Skills
+// ------
+var count = 0;
+
+var skills = [
+  "<span>H</span><span>T</span><span>M</span><span>L</span>",
+  "<span>C</span><span>S</span><span>S</span><span>/</span><span>S</span><span>A</span><span>S</span><span>S</span>",
+  "<span>J</span><span>a</span><span>v</span><span>a</span><span>s</span><span>c</span><span>r</span><span>i</span><span>p</span><span>t</span>",
+  "<span>R</span><span>u</span><span>b</span><span>y</span>&nbsp;<span>o</span><span>n</span>&nbsp;<span>R</span><span>a</span><span>i</span><span>l</span><span>s</span>",
+  "<span>R</span><span>e</span><span>a</span><span>c</span><span>t</span>&nbsp;<span>J</span><span>S</span>",
+  "<span>W</span><span>o</span><span>r</span><span>d</span><span>p</span><span>r</span><span>e</span><span>s</span><span>s</span>",
+  "<span>A</span><span>n</span><span>g</span><span>u</span><span>l</span><span>a</span><span>r</span>&nbsp;<span>J</span><span>S</span>",
+  "<span>C</span><span>r</span><span>a</span><span>f</span><span>t</span>&nbsp;<span>C</span><span>M</span><span>S</span>",
+  "<span>P</span><span>h</span><span>o</span><span>n</span><span>e</span><span>g</span><span>a</span><span>p</span>",
+  "<span>H</span><span>T</span><span>M</span><span>L</span>&nbsp;<span>E</span><span>m</span><span>a</span><span>i</span><span>l</span>",
+  "<span>P</span><span>h</span><span>o</span><span>t</span><span>o</span><span>s</span><span>h</span><span>o</span><span>p</span>",
+  "<span>S</span><span>k</span><span>e</span><span>t</span><span>c</span><span>h</span>"
+];
+
+var animateClass = [
+  'scale',
+  'slide-down',
+  'slide-up',
+  'slide-left',
+  'slide-right'
+];
+
+getSkill();
+setInterval('getSkill()', 2500);
+
+function getSkill() {
+  if(count < skills.length) {
+    var selected = skills[count];
+    var randomClass = animateClass[Math.floor(Math.random()*animateClass.length)];
+    console.log(randomClass);
+    $('#skills').attr('class', '');
+    $('#skills').addClass(randomClass);
+    $('#skills').html(selected);
+    count = count + 1;
+  } else {
+    count = 0;
+    getSkill();
+  }
+}
