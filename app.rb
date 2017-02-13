d3451acc2c85b235cd975a7101834667afe2ca4b
Adam <fidler2326@gmail.com>
2017-02-13 21:14:52 +0000

    Remove website from portfolio

--- app.rb
+++ app.rb
@@ -29,17 +29,17 @@ post '/' do
   end
 end
 
-def configure_pony
-  Pony.options = {
-    :via => :smtp,
-    :via_options => {
-      :address              => 'smtp.sendgrid.net',
-      :port                 => '587',
-      :user_name            => ENV['SENDGRID_USERNAME'],
-      :password             => ENV['SENDGRID_PASSWORD'],
-      :authentication       => :plain,
-      :enable_starttls_auto => true,
-      :domain               => 'heroku.com'
-    }
-  }
-end
+# def configure_pony
+#   Pony.options = {
+#     :via => :smtp,
+#     :via_options => {
+#       :address              => 'smtp.sendgrid.net',
+#       :port                 => '587',
+#       :user_name            => ENV['SENDGRID_USERNAME'],
+#       :password             => ENV['SENDGRID_PASSWORD'],
+#       :authentication       => :plain,
+#       :enable_starttls_auto => true,
+#       :domain               => 'heroku.com'
+#     }
+#   }
+# end
